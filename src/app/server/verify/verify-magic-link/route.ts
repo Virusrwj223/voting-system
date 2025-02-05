8aa533bc4aca01e68f96debf97d35242f0178e67
Virusrwj223 <mhrishiraj@gmail.com>
2025-02-05 09:23:08 +0800

    Fix import errors

--- src/app/server/verify/verify-magic-link/route.ts
+++ src/app/server/verify/verify-magic-link/route.ts
@@ -1,8 +1,7 @@
-'use server';
+"use server";
 
-import { NextRequest } from 'next/server';
-
-import { verifyMagicLink } from '@/controller/verify/verifyMagicLink';
+import { NextRequest } from "next/server";
+import { verifyMagicLink } from "../../../../../lib/controller/verify/verifyMagicLink";
 
 export async function GET(req: NextRequest) {
   return await verifyMagicLink(req);
--- src/app/server/verify/verify-user-access/route.ts
+++ src/app/server/verify/verify-user-access/route.ts
@@ -1,8 +1,7 @@
-'use server';
+"use server";
 
-import { NextRequest } from 'next/server';
-
-import { verifyUserAccess } from '@/controller/verify/verifyUserAccess';
+import { NextRequest } from "next/server";
+import { verifyUserAccess } from "../../../../../lib/controller/verify/verifyUserAccess";
 
 export async function GET(req: NextRequest) {
   return await verifyUserAccess(req);
