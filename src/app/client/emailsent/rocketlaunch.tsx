0eae3b89f1128fad408fbb372a3fac97a9c3520c
Virusrwj223 <mhrishiraj@gmail.com>
2025-02-05 14:09:14 +0800

    Improve Ui

--- src/app/client/emailsent/rocketlaunch.tsx
+++ src/app/client/emailsent/rocketlaunch.tsx
@@ -8,7 +8,7 @@ const RocketLaunch = () => {
     <div className="relative w-full h-screen bg-black flex flex-col items-center justify-center text-white font-sans">
       <motion.div
         initial={{ y: 300 }}
-        animate={{ y: -75 }}
+        animate={{ y: 25 }}
         transition={{ duration: 3, ease: "easeInOut" }}
         className="flex justify-center items-center bg-black w-[100px] h-[100px] rounded-full border-2 border-white"
       >
@@ -16,8 +16,8 @@ const RocketLaunch = () => {
       </motion.div>
 
       <motion.div
-        initial={{ opacity: 0, scale: 0 }}
-        animate={{ opacity: [0, 1, 0], scale: [0, 1, 0], y: -5 }}
+        initial={{ opacity: 0, scale: 0, y: 130 }}
+        animate={{ opacity: [0, 1, 0], scale: [0, 1, 0], y: 130 }}
         transition={{
           delay: 3,
           duration: 1.5,
@@ -27,14 +27,15 @@ const RocketLaunch = () => {
       ></motion.div>
 
       <motion.div
-        initial={{ opacity: 0, y: -87 }}
-        animate={{ opacity: 2, y: -87 }}
+        initial={{ opacity: 0, y: -5 }}
+        animate={{ opacity: 2, y: -5 }}
         transition={{ delay: 0.3, duration: 5, ease: "backIn" }}
         className="mt-12 text-center"
       >
         <p className="text-lg font-medium">
-          <strong>Voting Link</strong> is on its way. <br />
-          Please check your Inbox. You may need to refresh Inbox. <br />
+          <strong>Voting Link Incoming.</strong>
+          <br />
+          Please check Inbox. You may need to refresh Inbox. <br />
           Voting link expires in <strong>1 minute</strong>.
         </p>
       </motion.div>
